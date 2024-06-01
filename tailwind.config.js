9c39baa2a866194c53b9450c04afca4297a78409
Nate R <45110045+nathanroberts55@users.noreply.github.com>
2024-05-31 21:22:39 -0400

    Frontend Based on Mock (#1)
    * Im an indigo girlie
    
    * cleanup
    
    ---------
    
    Co-authored-by: Dylan Hackworth <me@dylhack.dev>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+		borderColor: (theme) => ({
+			...theme('colors'),
+			primary: '#F45B69'
+		})
+	},
+	plugins: []
+};

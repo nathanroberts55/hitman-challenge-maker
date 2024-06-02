8fa67d31219aabca12361844e69c13a5e686ec20
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-06-02 00:11:48 -0400

    Entree for the Day

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,6 @@
 <script>
-	import List from './List.svelte';
-	import * as hitman from '$lib/hitman';
-
-	const MAX_CHALLENGES = 5;
-	const { map, difficulty, challenges, weapons } = hitman.randomScenario({
-		weapons: 3,
-		challenges: Math.floor(Math.random() * MAX_CHALLENGES) + 1
-	});
+    import * as hitman from '$lib/hitman';
+    export const maps = hitman.MAPS
 </script>
 
 <svelte:head>
@@ -14,21 +8,14 @@
 	<meta name="description" content="King of the Kill" />
 </svelte:head>
 
-
-<div class="flex max-md:flex-col gap-4 my-auto mx-auto">
-	<div class="max-w-sm rounded-xl overflow-hidden shadow-lg border-8 border-primary max-md:w-full max-md:h-[350px] lg:w-[395px] lg:h-[600px]">
-		<!-- Map Image -->
-		<!-- Placeholder Image -->
-		<img src="https://cdna.artstation.com/p/assets/images/images/014/447/884/large/alex-jareno-elite3d-hitman2-miami-06.jpg?1544010665" alt="Temp" class="object-cover h-full">
-	</div>
-	<div class="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white max-md:w-full lg:w-[395px] lg:h-[600px] p-8">
-		<h1 class="text-5xl font-bold underline">{map.name}</h1>
-		<div class="flex flex-col gap-3">
-			<p class="font-bold">Difficulty: {difficulty}</p>
-			<List name="Targets" items={map.targets} />
-			<List name="Challenge" items={challenges} />
-			<List name="Weapons" items={weapons} />
-		</div>
-	</div>
+<div class="flex flex-col gap-4 my-auto mx-auto">
+    <div class="max-w-sm rounded-xl overflow-hidden shadow-lg border-8 border-primary my-4 max-md:w-full max-md:h-[350px] lg:w-[395px] lg:h-[500px]">
+        <img src="https://cdna.artstation.com/p/assets/images/images/014/447/884/large/alex-jareno-elite3d-hitman2-miami-06.jpg?1544010665" alt="Temp" class="object-cover h-full">
+    </div>
+    <select name="MapSelect" id="mapSelect" placeholder="Select Map" class="rounded-md bg-gray-500 text-white text-center h-8 w-3/4 mx-auto my-auto">
+        <option value="" disabled selected>Select Map</option>
+        {#each maps as map}
+			<option value="{map.name}">{map.name}</option>
+		{/each}
+    </select>
 </div>
-
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Toggle from '../Toggle.svelte';
+    import * as hitman from '$lib/hitman';
+    export const maps = hitman.MAPS
+</script>
+
+<svelte:head>
+	<title>Settings - King of the Kill</title>
+	<meta name="description" content="King of the Kill" />
+</svelte:head>
+
+<div class="fixed top-0 left-0 h-screen w-screen flex flex-row">
+    <div class="w-4/6 h-full bg-[#152430]">
+        <img class="object-cover h-full w-full" src="https://cdna.artstation.com/p/assets/images/images/014/447/884/large/alex-jareno-elite3d-hitman2-miami-06.jpg?1544010665" alt="Temp">
+    </div>
+    <div class="w-2/6 h-full px-2 py-8 text-center bg-[#152430] relative">
+        <p class="text-4xl text-white my-8">Challenge Settings</p>
+        <div class="space-y-2">
+            <Toggle name="Weapons Challenge" />
+            <Toggle name="Time Challenge" />
+            <Toggle name="Traversal Challenge" />
+            <Toggle name="Kill Challenge" />
+            <Toggle name="Disguise Challenge" />
+        </div>
+        <div class="flex flex-row gap-4 absolute bottom-8 inset-x-28 justify-items-center">
+            <button class="py-2 px-4 rounded-lg bg-blue-400 text-white text-xl">Start Challenge</button>
+            <a class="btn py-2 px-4 rounded-lg bg-blue-400 text-white" href="/challenge">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                </svg>
+            </a>     
+        </div>
+    </div>
+</div>

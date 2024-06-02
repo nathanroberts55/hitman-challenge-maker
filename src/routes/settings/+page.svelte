d55495c20baf2035a7f6be3dfee82ed3a4925a94
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-06-02 14:51:32 -0400

    Added Mobile View

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -9,11 +9,21 @@
 	<meta name="description" content="King of the Kill" />
 </svelte:head>
 
-<div class="fixed top-0 left-0 h-screen w-screen flex flex-row">
-	<div class="w-4/6 h-full bg-[#152430]">
-		<img class="object-cover h-full w-full" src="/maps/miami.jpg" alt="Temp" />
+<div class="fixed top-0 left-0 h-screen w-screen flex max-md:flex-col">
+	<div class="max-md:w-full max-md:h-2/6 w-4/6 h-full bg-[url(/maps/miami.jpg)] flex justify-center content-end gap-4 bg-no-repeat bg-cover">
+		<p class="mb-4 my-auto py-2 px-4 h-12 rounded-lg bg-blue-400 text-white text-xl">{maps[0].name}</p>
+		<button class="mb-4 my-auto py-2 px-4 h-12 rounded-lg bg-blue-400 text-white">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+			</svg>
+		</button>
+		<button class="mb-4 my-auto py-2 px-4 h-12 rounded-lg bg-blue-400 text-white">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+			  </svg>			  
+		</button>
 	</div>
-	<div class="w-2/6 h-full px-2 py-8 text-center bg-[#152430] relative">
+	<div class="max-md:w-full max-md:h-4/6 w-2/6 h-full px-2 py-8 text-center bg-[#152430] relative">
 		<p class="text-4xl text-white my-8">Challenge Settings</p>
 		<div class="space-y-2">
 			<Toggle label="Weapons Challenge" name="weapons" />
@@ -22,7 +32,7 @@
 			<Toggle label="Kill Challenge" name="kills" />
 			<Toggle label="Disguise Challenge" name="disguise" />
 		</div>
-		<div class="flex flex-row gap-4 absolute bottom-8 inset-x-28 justify-items-center">
+		<div class="flex flex-row gap-4 absolute bottom-8 max-md:inset-x-16 inset-x-28 justify-items-center">
 			<button class="py-2 px-4 rounded-lg bg-blue-400 text-white text-xl">Start Challenge</button>
 			<a class="btn py-2 px-4 rounded-lg bg-blue-400 text-white" href="/challenge">
 				<svg
